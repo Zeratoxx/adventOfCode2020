60e51af9c830a9ff6efa3c500a5e872c8a3471c1
Nils Badtke <nils.badtke@stud.h-da.de>
2020-12-07 17:59:30 +0100

    day 7 part two finish

--- day_7/main.py
+++ day_7/main.py
@@ -1,7 +1,7 @@
 from day_7.bag import Bag
 from day_7.rule import Rule
 from day_7.search_engine import search_for_bags_that_can_contain_given_bag_recursively, \
-    get_amount_of_bags_that_given_bag_contains_recursively
+    get_amount_of_bags_that_given_bag_contains_recursively_start, complete_tree
 
 
 def get_data(input_path):
@@ -34,9 +34,73 @@ def run():
 
     print("")
     print("")
-    sum_of_bags = get_amount_of_bags_that_given_bag_contains_recursively(rules, {Bag("shiny", "gold"): 1})
+    sum_of_bags = get_amount_of_bags_that_given_bag_contains_recursively_start(rules, {Bag("shiny", "gold"): 1})
+    for line in reversed(complete_tree):
+        print(line)
+    print(f'sum_of_bags: {sum_of_bags}')
+
+
+def run2():
+    data = get_data("./data/example")
+    rules = []
+    for line in data:
+        rules.append(Rule(line))
+
+    print(f'data: {data}')
+    print(f'last data entry: {data[-1]}')
+    print(f'lenght: {len(data)}')
+    print("")
+    for rule in rules:
+        print(rule.__str__())
+    print(f'lenght: {len(rules)}')
+
+    # print("")
+    # print("")
+    # valid_bags = search_for_bags_that_can_contain_given_bag_recursively(rules, [Bag("shiny", "gold")])
+    # for bag in valid_bags:
+    #     print(bag)
+    # print("")
+    # print(f'lenght: {len(valid_bags)}')
+
+    print("")
+    print("")
+    sum_of_bags = get_amount_of_bags_that_given_bag_contains_recursively_start(rules, {Bag("shiny", "gold"): 1})
+    for line in reversed(complete_tree):
+        print(line)
+    print(f'sum_of_bags: {sum_of_bags}')
+
+
+def run3():
+    data = get_data("./data/example2")
+    rules = []
+    for line in data:
+        rules.append(Rule(line))
+
+    print(f'data: {data}')
+    print(f'last data entry: {data[-1]}')
+    print(f'lenght: {len(data)}')
+    print("")
+    for rule in rules:
+        print(rule.__str__())
+    print(f'lenght: {len(rules)}')
+
+    # print("")
+    # print("")
+    # valid_bags = search_for_bags_that_can_contain_given_bag_recursively(rules, [Bag("shiny", "gold")])
+    # for bag in valid_bags:
+    #     print(bag)
+    # print("")
+    # print(f'lenght: {len(valid_bags)}')
+
+    print("")
+    print("")
+    sum_of_bags = get_amount_of_bags_that_given_bag_contains_recursively_start(rules, {Bag("shiny", "gold"): 1})
+    for line in reversed(complete_tree):
+        print(line)
     print(f'sum_of_bags: {sum_of_bags}')
 
 
 if __name__ == '__main__':
     run()
+    # run2()
+    # run3()
--- day_7/search_engine.py
+++ day_7/search_engine.py
@@ -1,3 +1,6 @@
+complete_tree = []
+
+
 def search_for_bags_that_can_contain_given_bag_recursively(rules, searched_bags):
     valid_bags = []
     for rule in rules:
@@ -11,17 +14,43 @@ def search_for_bags_that_can_contain_given_bag_recursively(rules, searched_bags)
     return valid_bags
 
 
-def get_amount_of_bags_that_given_bag_contains_recursively(rules, searched_bags_dict):
+def get_amount_of_bags_that_given_bag_contains_recursively_start(rules, searched_bags_dict):
     rules_dict = {}
     for rule in rules:
         rules_dict[rule.bag] = rule.rule_restrictions
 
-    amount = 0
+    complete_tree = []
+
+    amount = get_amount_of_bags_that_given_bag_contains_recursively(rules_dict, searched_bags_dict, 0)
     for bag in searched_bags_dict:
+        amount -= searched_bags_dict[bag]
+    return amount
+
+
+def get_amount_of_bags_that_given_bag_contains_recursively(rules_dict, searched_bags_dict, depth):
+    tree_part = "|   "
+    tree = ""
+    amount = 0
+    amount_next = 1
+    amount_below = 0
+    for bag in searched_bags_dict:
+        for x in range(depth):
+            tree += tree_part
+        depth += 1
         if len(rules_dict[bag]) == 0:
-            amount += searched_bags_dict[bag] * 1
+            amount += searched_bags_dict[bag] * amount_next
         else:
+            amount += searched_bags_dict[bag]
             for rule_rest in rules_dict[bag]:
-                amount += searched_bags_dict[bag] * \
-                          get_amount_of_bags_that_given_bag_contains_recursively(rules, {rule_rest.bag: rule_rest.amount})
+                amount_next = get_amount_of_bags_that_given_bag_contains_recursively(
+                    rules_dict,
+                    {rule_rest.bag: rule_rest.amount},
+                    depth
+                )
+                amount_below += amount_next
+                amount += searched_bags_dict[bag] * amount_next
+        if amount_below == 0:
+            amount_below = 1
+        complete_tree.append(tree + str(amount) + " = " + str(searched_bags_dict[bag]) + " + " +
+                             str(searched_bags_dict[bag]) + " x " + str(amount_below) + " ; " + str(bag.__str__()))
     return amount
